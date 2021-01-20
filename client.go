fd200d9bca28408d6f30f9ffb3f2f58b4e367358
むずでょ <muzudho1@gmail.com>
2021-01-20 20:00:12 +0900

    OK

---
+++ client.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"os"
+
+	"github.com/reiver/go-oi"
+	"github.com/reiver/go-telnet"
+)
+
+func startClient() error {
+	return telnet.DialToAndCall("localhost:5555", clientListener{})
+}
+
+type clientListener struct{}
+
+// CallTELNET - 決まった形のメソッド。
+func (c clientListener) CallTELNET(ctx telnet.Context, w telnet.Writer, r telnet.Reader) {
+	// scanner - 標準入力を監視します。
+	scanner := bufio.NewScanner(os.Stdin)
+	// 一行読み取ります。
+	for scanner.Scan() {
+		// 書き込みます。最後に改行を付けます。
+		oi.LongWrite(w, scanner.Bytes())
+		oi.LongWrite(w, []byte("\n"))
+	}
+}
